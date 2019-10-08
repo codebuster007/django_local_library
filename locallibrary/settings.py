6f09fe4254e71390c9bb3217b62dcba4e70221b1
JOMADI <victorjo603@gmail.com>
2019-10-08 13:45:07 +0100

    Added files and changes required for deployment

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -117,8 +118,7 @@ USE_TZ = True
 LOGIN_REDIRECT_URL = '/'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-# Heroku: Update database configuration from $DATABASE_URL.
-import dj_database_url
+
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
